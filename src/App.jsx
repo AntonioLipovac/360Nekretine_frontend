596a16428c1f63ae7cdd77a54d58aa657c9f2634
AntonioLipovac <75137131+AntonioLipovac@users.noreply.github.com>
2024-06-18 02:07:15 +0200

    Dodano: Pages, routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Pocetna from './pages/Pocetna';
+import Prijava from './pages/Prijava';
+import Registracija from './pages/Registracija';
+import ONama from './pages/ONama';
+import Profil from './pages/Profil';
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+
+    <Route path='/' element={<Pocetna />} />
+    <Route path='/prijava' element={<Prijava />} />
+    <Route path='/registracija' element={<Registracija />} />
+    <Route path='/o-nama' element={<ONama />} />
+    <Route path='/profil' element={<Profil />} />
+
+
+    </Routes>
+    </BrowserRouter>
   )
 }
