2d872effe01b074c89858c4b5b8c1b32653168d1
AntonioLipovac <75137131+AntonioLipovac@users.noreply.github.com>
2024-09-04 12:16:27 +0200

    Spajanje na Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://nekretnine360-backend.onrender.com',
         secure: false,
       },
     },
